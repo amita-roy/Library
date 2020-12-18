971e1686a3cd9e8d2b04529470613a18ea3f192e
Amita <sardana.amita2@gmail.com>
2020-12-18 15:34:41 +0530

    Seprate the bookCard in its own function

--- Js/main.js
+++ Js/main.js
@@ -17,30 +17,34 @@ function addBookToLibrary(title, author, pages, read) {
 
 let content = '';
 
+function bookCard(book) {
+  return `
+  <div id="book" class="col-md-4 mt-2">
+      <div class="card" style="width: 18rem;">
+          <div class="card-body">
+              <h5 class="card-title" id="itemName">${book.title}</h5>
+              <p class="card-text" id="itemDesc">${book.author}</p>
+              <p class="card-text">${book.pages}</p>
+              <p class="card-text">${book.read}</p>
+              <a href="#" class="btn btn-primary" id="addCart">Delete</a>
+          </div>
+      </div>
+    </div>
+`;
+}
+
 function displayAllBooks(books) {
   books.forEach((book) => {
-    content += `
-    <div id="book" class="col-md-4 mt-2">
-        <div class="card" style="width: 18rem;">
-            <div class="card-body">
-                <h5 class="card-title" id="itemName">${book.title}</h5>
-                <p class="card-text" id="itemDesc">${book.author}</p>
-                <p class="card-text">${book.pages}</p>
-                <p class="card-text">${book.read}</p>
-                <a href="#" class="btn btn-primary" id="addCart">Delete</a>
-            </div>
-        </div>
-      </div>
-  `;
+    content += bookCard(book);
   });
 }
 
-addBookToLibrary("Anything", "Me", "unlimited", "read");
-addBookToLibrary("Elias", "Me", "500", "read");
-addBookToLibrary("Amita", "You", "300", "not read");
-addBookToLibrary("VsCode", "It", "100", "not read");
+addBookToLibrary('Anything', 'Me', 'unlimited', 'read');
+addBookToLibrary('Elias', 'Me', '500', 'read');
+addBookToLibrary('Amita', 'You', '300', 'not read');
+addBookToLibrary('VsCode', 'It', '100', 'not read');
 
 displayAllBooks(myLibrary);
-document.querySelector(".books-grid").innerHTML = content;
+document.querySelector('.books-grid').innerHTML = content;
 
 console.log(content);
