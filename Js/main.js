8d6809fac18ef85f6e962bd548f1dda16fd6ecfb
Amita-Roy <sardana.amita2@gmail.com>
2021-01-12 12:55:53 +0100

    Update delete and status button

--- Js/main.js
+++ Js/main.js
@@ -1,7 +1,8 @@
 let myLibrary = [];
 let addNewBookButton = document.querySelector('button.newBook');
 
-function Book(title, author, pages) {
+function Book(title, author, pages, id) {
+  this.id = id;
   this.title = title;
   this.author = author;
   this.pages = pages;
@@ -12,8 +13,8 @@ Book.prototype.info = function () {
 
 Book.prototype.read = 'Not Read';
 
-function addBookToLibrary(title, author, pages) {
-  let newBook = new Book(title, author, pages);
+function addBookToLibrary(title, author, pages, id) {
+  let newBook = new Book(title, author, pages, id);
   myLibrary.push(newBook);
 }
 
@@ -21,14 +22,14 @@ let content = '';
 
 function bookCard(book, index) {
   return `
-  <div id="book" class="col-md-4 mt-2">
+  <div id="book-${index}" class="col-md-4 mt-2">
       <div class="card" style="width: 18rem;">
           <div class="card-body">
               <h5 class="card-title" id="itemName">${book.title}</h5>
               <p class="card-text" id="itemDesc">${book.author}</p>
               <p class="card-text">${book.pages}</p>
               <button class="card-text status">${book.read}</button>
-              <button class="delete" data-id=${index}>Delete</button>
+              <button class="delete" id=${index} data-id=${index}>Delete</button>
           </div>
       </div>
     </div>
@@ -45,27 +46,33 @@ $('form').on('submit', function (event) {
   event.preventDefault();
   content = '';
   let test = $(this).serializeArray();
-
-  addBookToLibrary(test[0].value, test[1].value, test[2].value);
+  let index = myLibrary.length;
+  addBookToLibrary(test[0].value, test[1].value, test[2].value, index);
   displayAllBooks(myLibrary);
   document.querySelector('.books-grid').innerHTML = content;
   this.reset();
+  console.log(index);
 
-  let deleteButton = document.querySelector('button.delete');
-  let statusButton = document.querySelector('button.status');
+  myLibrary.forEach((book, index) => {
+    let deleteButton = document.querySelector(`#book-${index} button.delete`);
+    let statusButton = document.querySelector(`#book-${index} button.status`);
 
-  deleteButton.addEventListener('click', function () {
-    content = '';
-    console.log('BEFORE', myLibrary);
-    id = this.dataset.id;
-    myLibrary = myLibrary.filter((_, index) => index != id);
-    console.log('AFTER', myLibrary);
-    displayAllBooks(myLibrary);
-    document.querySelector('.books-grid').innerHTML = content;
-  });
-
-  statusButton.addEventListener('click', function () {
-    $(this).html() === 'Read' ? $(this).html('Not Read') : $(this).html('Read');
+    deleteButton.addEventListener('click', function () {
+      // content = '';
+      console.log('BEFORE', myLibrary);
+      id = this.dataset.id;
+      myLibrary = myLibrary.filter((book) => book.id !== index);
+      console.log('AFTER', myLibrary);
+      const card = document.getElementById(`book-${index}`);
+      if (card) {
+        card.remove();
+      }
+    });
+    statusButton.addEventListener('click', function () {
+      $(this).html() === 'Read'
+        ? $(this).html('Not Read')
+        : $(this).html('Read');
+    });
   });
 
   if ($('.form').hasClass('show')) {
